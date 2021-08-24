4bce39373ea1157854190618239d201482a7a461
冷淡然 <lengdanran@126.com>
2021-08-24 13:37:30 +0800

    添加RpcServer顶部类

---
+++ src/main/java/danran/rpc/server/RpcServer.java
@@ -0,0 +1,66 @@
+package danran.rpc.server;
+
+
+/**
+ * RPC服务端抽象类
+ *
+ */
+public abstract class RpcServer {
+
+    /**
+     * 服务端口
+     */
+    protected int port;
+
+    /**
+     * 服务协议
+     */
+    protected String protocol;
+
+    /**
+     * 请求处理者
+     */
+    protected RequestHandler handler;
+
+    public RpcServer(int port, String protocol, RequestHandler handler) {
+        super();
+        this.port = port;
+        this.protocol = protocol;
+        this.handler = handler;
+    }
+
+    /**
+     * 开启服务
+     */
+    public abstract void start();
+
+    /**
+     * 停止服务
+     */
+    public abstract void stop();
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public RequestHandler getHandler() {
+        return handler;
+    }
+
+    public void setHandler(RequestHandler handler) {
+        this.handler = handler;
+    }
+
+}
